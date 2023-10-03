96840d5dc08288b74d0403cac2a7f8086e1d3c70
Mogora <67151047+Mogora@users.noreply.github.com>
2023-10-03 20:45:52 +0300

    3.10.2023 Реализована верстка ItemsTab

--- ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -33,7 +33,9 @@ namespace ObjectOrientedPractics
             this.ItemsTabPage = new System.Windows.Forms.TabPage();
             this.CustomerstabPage = new System.Windows.Forms.TabPage();
             this.customersTabs1 = new ObjectOrientedPractics.View.Tabs.CustomersTab();
+            this.itemsTab1 = new ObjectOrientedPractics.View.Tabs.ItemsTab();
             this.tabControl.SuspendLayout();
+            this.ItemsTabPage.SuspendLayout();
             this.CustomerstabPage.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,6 +52,7 @@ namespace ObjectOrientedPractics
             // 
             // ItemsTabPage
             // 
+            this.ItemsTabPage.Controls.Add(this.itemsTab1);
             this.ItemsTabPage.Location = new System.Drawing.Point(4, 25);
             this.ItemsTabPage.Name = "ItemsTabPage";
             this.ItemsTabPage.Padding = new System.Windows.Forms.Padding(3);
@@ -71,11 +74,20 @@ namespace ObjectOrientedPractics
             // 
             // customersTabs1
             // 
+            this.customersTabs1.Customers = null;
             this.customersTabs1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.customersTabs1.Location = new System.Drawing.Point(3, 3);
             this.customersTabs1.Name = "customersTabs1";
             this.customersTabs1.Size = new System.Drawing.Size(825, 509);
             this.customersTabs1.TabIndex = 0;
+            // 
+            // itemsTab1
+            // 
+            this.itemsTab1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.itemsTab1.Location = new System.Drawing.Point(3, 3);
+            this.itemsTab1.Name = "itemsTab1";
+            this.itemsTab1.Size = new System.Drawing.Size(825, 509);
+            this.itemsTab1.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -86,6 +98,7 @@ namespace ObjectOrientedPractics
             this.Name = "MainForm";
             this.Text = "Object Oriented Practics";
             this.tabControl.ResumeLayout(false);
+            this.ItemsTabPage.ResumeLayout(false);
             this.CustomerstabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -97,6 +110,7 @@ namespace ObjectOrientedPractics
         private System.Windows.Forms.TabPage ItemsTabPage;
         private System.Windows.Forms.TabPage CustomerstabPage;
         private View.Tabs.CustomersTab customersTabs1;
+        private View.Tabs.ItemsTab itemsTab1;
     }
 }
 
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/ItemsTab.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class ItemsTab
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemsPanel = new System.Windows.Forms.Panel();
+            this.ButtonsPanel = new System.Windows.Forms.Panel();
+            this.RemovePanel = new System.Windows.Forms.Panel();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.AddPanel = new System.Windows.Forms.Panel();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ItemsListBox = new System.Windows.Forms.ListBox();
+            this.ItemsLabel = new System.Windows.Forms.Label();
+            this.SelectedItemPanel = new System.Windows.Forms.Panel();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.FullNameTextBox = new System.Windows.Forms.TextBox();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.SelectedCustomerLabel = new System.Windows.Forms.Label();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.ItemsPanel.SuspendLayout();
+            this.ButtonsPanel.SuspendLayout();
+            this.RemovePanel.SuspendLayout();
+            this.AddPanel.SuspendLayout();
+            this.SelectedItemPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ItemsPanel
+            // 
+            this.ItemsPanel.Controls.Add(this.ButtonsPanel);
+            this.ItemsPanel.Controls.Add(this.ItemsListBox);
+            this.ItemsPanel.Controls.Add(this.ItemsLabel);
+            this.ItemsPanel.Location = new System.Drawing.Point(3, 3);
+            this.ItemsPanel.Name = "ItemsPanel";
+            this.ItemsPanel.Size = new System.Drawing.Size(370, 509);
+            this.ItemsPanel.TabIndex = 1;
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.Controls.Add(this.RemovePanel);
+            this.ButtonsPanel.Controls.Add(this.AddPanel);
+            this.ButtonsPanel.Location = new System.Drawing.Point(6, 442);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.Size = new System.Drawing.Size(361, 64);
+            this.ButtonsPanel.TabIndex = 2;
+            // 
+            // RemovePanel
+            // 
+            this.RemovePanel.Controls.Add(this.RemoveButton);
+            this.RemovePanel.Location = new System.Drawing.Point(136, 3);
+            this.RemovePanel.Name = "RemovePanel";
+            this.RemovePanel.Size = new System.Drawing.Size(127, 58);
+            this.RemovePanel.TabIndex = 2;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(3, 3);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(121, 52);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // AddPanel
+            // 
+            this.AddPanel.Controls.Add(this.AddButton);
+            this.AddPanel.Location = new System.Drawing.Point(3, 3);
+            this.AddPanel.Name = "AddPanel";
+            this.AddPanel.Size = new System.Drawing.Size(127, 58);
+            this.AddPanel.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(3, 3);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(121, 52);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemsListBox
+            // 
+            this.ItemsListBox.FormattingEnabled = true;
+            this.ItemsListBox.ItemHeight = 16;
+            this.ItemsListBox.Location = new System.Drawing.Point(6, 32);
+            this.ItemsListBox.Name = "ItemsListBox";
+            this.ItemsListBox.Size = new System.Drawing.Size(361, 404);
+            this.ItemsListBox.TabIndex = 1;
+            // 
+            // ItemsLabel
+            // 
+            this.ItemsLabel.AutoSize = true;
+            this.ItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ItemsLabel.Location = new System.Drawing.Point(6, 12);
+            this.ItemsLabel.Name = "ItemsLabel";
+            this.ItemsLabel.Size = new System.Drawing.Size(46, 17);
+            this.ItemsLabel.TabIndex = 0;
+            this.ItemsLabel.Text = "Items";
+            // 
+            // SelectedItemPanel
+            // 
+            this.SelectedItemPanel.Controls.Add(this.DescriptionTextBox);
+            this.SelectedItemPanel.Controls.Add(this.DescriptionLabel);
+            this.SelectedItemPanel.Controls.Add(this.NameTextBox);
+            this.SelectedItemPanel.Controls.Add(this.NameLabel);
+            this.SelectedItemPanel.Controls.Add(this.FullNameTextBox);
+            this.SelectedItemPanel.Controls.Add(this.CostLabel);
+            this.SelectedItemPanel.Controls.Add(this.IdTextBox);
+            this.SelectedItemPanel.Controls.Add(this.SelectedCustomerLabel);
+            this.SelectedItemPanel.Controls.Add(this.IdLabel);
+            this.SelectedItemPanel.Location = new System.Drawing.Point(376, 3);
+            this.SelectedItemPanel.Name = "SelectedItemPanel";
+            this.SelectedItemPanel.Size = new System.Drawing.Size(452, 509);
+            this.SelectedItemPanel.TabIndex = 2;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(9, 129);
+            this.NameTextBox.Multiline = true;
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(434, 123);
+            this.NameTextBox.TabIndex = 8;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(6, 109);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(49, 17);
+            this.NameLabel.TabIndex = 7;
+            this.NameLabel.Text = "Name:";
+            // 
+            // FullNameTextBox
+            // 
+            this.FullNameTextBox.Location = new System.Drawing.Point(87, 70);
+            this.FullNameTextBox.Name = "FullNameTextBox";
+            this.FullNameTextBox.Size = new System.Drawing.Size(131, 22);
+            this.FullNameTextBox.TabIndex = 6;
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Location = new System.Drawing.Point(6, 70);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(40, 17);
+            this.CostLabel.TabIndex = 5;
+            this.CostLabel.Text = "Cost:";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Location = new System.Drawing.Point(87, 42);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.ReadOnly = true;
+            this.IdTextBox.Size = new System.Drawing.Size(131, 22);
+            this.IdTextBox.TabIndex = 4;
+            // 
+            // SelectedCustomerLabel
+            // 
+            this.SelectedCustomerLabel.AutoSize = true;
+            this.SelectedCustomerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectedCustomerLabel.Location = new System.Drawing.Point(6, 12);
+            this.SelectedCustomerLabel.Name = "SelectedCustomerLabel";
+            this.SelectedCustomerLabel.Size = new System.Drawing.Size(144, 17);
+            this.SelectedCustomerLabel.TabIndex = 3;
+            this.SelectedCustomerLabel.Text = "Selected Customer";
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(6, 42);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(25, 17);
+            this.IdLabel.TabIndex = 0;
+            this.IdLabel.Text = "ID:";
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(6, 268);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(83, 17);
+            this.DescriptionLabel.TabIndex = 9;
+            this.DescriptionLabel.Text = "Description:";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(9, 288);
+            this.DescriptionTextBox.Multiline = true;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(434, 123);
+            this.DescriptionTextBox.TabIndex = 10;
+            // 
+            // ItemsTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SelectedItemPanel);
+            this.Controls.Add(this.ItemsPanel);
+            this.Name = "ItemsTab";
+            this.Size = new System.Drawing.Size(831, 515);
+            this.ItemsPanel.ResumeLayout(false);
+            this.ItemsPanel.PerformLayout();
+            this.ButtonsPanel.ResumeLayout(false);
+            this.RemovePanel.ResumeLayout(false);
+            this.AddPanel.ResumeLayout(false);
+            this.SelectedItemPanel.ResumeLayout(false);
+            this.SelectedItemPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ItemsPanel;
+        private System.Windows.Forms.Panel ButtonsPanel;
+        private System.Windows.Forms.Panel RemovePanel;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Panel AddPanel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.ListBox ItemsListBox;
+        private System.Windows.Forms.Label ItemsLabel;
+        private System.Windows.Forms.Panel SelectedItemPanel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox FullNameTextBox;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.Label SelectedCustomerLabel;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Label DescriptionLabel;
+    }
+}
