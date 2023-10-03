7a79ace7c4df8d4e17cb9c3ab7aa440b10b9dc22
Mogora <67151047+Mogora@users.noreply.github.com>
2023-10-03 20:11:25 +0300

    03.10.2023. Реализована верстка у CustomersTab

--- ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -29,13 +29,61 @@ namespace ObjectOrientedPractics
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.ItemsTabPage = new System.Windows.Forms.TabPage();
+            this.CustomerstabPage = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.ItemsTabPage);
+            this.tabControl.Controls.Add(this.CustomerstabPage);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(839, 544);
+            this.tabControl.TabIndex = 0;
+            // 
+            // ItemsTabPage
+            // 
+            this.ItemsTabPage.Location = new System.Drawing.Point(4, 25);
+            this.ItemsTabPage.Name = "ItemsTabPage";
+            this.ItemsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ItemsTabPage.Size = new System.Drawing.Size(831, 515);
+            this.ItemsTabPage.TabIndex = 0;
+            this.ItemsTabPage.Text = "Items";
+            this.ItemsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // CustomerstabPage
+            // 
+            this.CustomerstabPage.Location = new System.Drawing.Point(4, 25);
+            this.CustomerstabPage.Name = "CustomerstabPage";
+            this.CustomerstabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.CustomerstabPage.Size = new System.Drawing.Size(792, 421);
+            this.CustomerstabPage.TabIndex = 1;
+            this.CustomerstabPage.Text = "Customers";
+            this.CustomerstabPage.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(839, 544);
+            this.Controls.Add(this.tabControl);
+            this.Name = "MainForm";
+            this.Text = "Object Oriented Practics";
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage ItemsTabPage;
+        private System.Windows.Forms.TabPage CustomerstabPage;
     }
 }
 
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CustomersTabs.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class CustomersTabs
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CustomersPanel = new System.Windows.Forms.Panel();
+            this.CustomersLabel = new System.Windows.Forms.Label();
+            this.CustomersListBox = new System.Windows.Forms.ListBox();
+            this.ButtonsPanel = new System.Windows.Forms.Panel();
+            this.AddPanel = new System.Windows.Forms.Panel();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemovePanel = new System.Windows.Forms.Panel();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.SelectedCustomerPanel = new System.Windows.Forms.Panel();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.SelectedCustomerLabel = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.FullNameLabel = new System.Windows.Forms.Label();
+            this.FullNameTextBox = new System.Windows.Forms.TextBox();
+            this.AddressLabel = new System.Windows.Forms.Label();
+            this.AddresTextBox = new System.Windows.Forms.TextBox();
+            this.CustomersPanel.SuspendLayout();
+            this.ButtonsPanel.SuspendLayout();
+            this.AddPanel.SuspendLayout();
+            this.RemovePanel.SuspendLayout();
+            this.SelectedCustomerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CustomersPanel
+            // 
+            this.CustomersPanel.Controls.Add(this.ButtonsPanel);
+            this.CustomersPanel.Controls.Add(this.CustomersListBox);
+            this.CustomersPanel.Controls.Add(this.CustomersLabel);
+            this.CustomersPanel.Location = new System.Drawing.Point(3, 3);
+            this.CustomersPanel.Name = "CustomersPanel";
+            this.CustomersPanel.Size = new System.Drawing.Size(370, 509);
+            this.CustomersPanel.TabIndex = 0;
+            // 
+            // CustomersLabel
+            // 
+            this.CustomersLabel.AutoSize = true;
+            this.CustomersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CustomersLabel.Location = new System.Drawing.Point(6, 12);
+            this.CustomersLabel.Name = "CustomersLabel";
+            this.CustomersLabel.Size = new System.Drawing.Size(84, 17);
+            this.CustomersLabel.TabIndex = 0;
+            this.CustomersLabel.Text = "Customers";
+            // 
+            // CustomersListBox
+            // 
+            this.CustomersListBox.FormattingEnabled = true;
+            this.CustomersListBox.ItemHeight = 16;
+            this.CustomersListBox.Location = new System.Drawing.Point(6, 32);
+            this.CustomersListBox.Name = "CustomersListBox";
+            this.CustomersListBox.Size = new System.Drawing.Size(361, 404);
+            this.CustomersListBox.TabIndex = 1;
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.Controls.Add(this.RemovePanel);
+            this.ButtonsPanel.Controls.Add(this.AddPanel);
+            this.ButtonsPanel.Location = new System.Drawing.Point(6, 442);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.Size = new System.Drawing.Size(361, 64);
+            this.ButtonsPanel.TabIndex = 2;
+            // 
+            // AddPanel
+            // 
+            this.AddPanel.Controls.Add(this.AddButton);
+            this.AddPanel.Location = new System.Drawing.Point(3, 3);
+            this.AddPanel.Name = "AddPanel";
+            this.AddPanel.Size = new System.Drawing.Size(127, 58);
+            this.AddPanel.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(3, 3);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(121, 52);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // RemovePanel
+            // 
+            this.RemovePanel.Controls.Add(this.RemoveButton);
+            this.RemovePanel.Location = new System.Drawing.Point(136, 3);
+            this.RemovePanel.Name = "RemovePanel";
+            this.RemovePanel.Size = new System.Drawing.Size(127, 58);
+            this.RemovePanel.TabIndex = 2;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(3, 3);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(121, 52);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // SelectedCustomerPanel
+            // 
+            this.SelectedCustomerPanel.Controls.Add(this.AddresTextBox);
+            this.SelectedCustomerPanel.Controls.Add(this.AddressLabel);
+            this.SelectedCustomerPanel.Controls.Add(this.FullNameTextBox);
+            this.SelectedCustomerPanel.Controls.Add(this.FullNameLabel);
+            this.SelectedCustomerPanel.Controls.Add(this.IdTextBox);
+            this.SelectedCustomerPanel.Controls.Add(this.SelectedCustomerLabel);
+            this.SelectedCustomerPanel.Controls.Add(this.IdLabel);
+            this.SelectedCustomerPanel.Location = new System.Drawing.Point(379, 3);
+            this.SelectedCustomerPanel.Name = "SelectedCustomerPanel";
+            this.SelectedCustomerPanel.Size = new System.Drawing.Size(449, 308);
+            this.SelectedCustomerPanel.TabIndex = 1;
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(6, 42);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(25, 17);
+            this.IdLabel.TabIndex = 0;
+            this.IdLabel.Text = "ID:";
+            // 
+            // SelectedCustomerLabel
+            // 
+            this.SelectedCustomerLabel.AutoSize = true;
+            this.SelectedCustomerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectedCustomerLabel.Location = new System.Drawing.Point(6, 12);
+            this.SelectedCustomerLabel.Name = "SelectedCustomerLabel";
+            this.SelectedCustomerLabel.Size = new System.Drawing.Size(144, 17);
+            this.SelectedCustomerLabel.TabIndex = 3;
+            this.SelectedCustomerLabel.Text = "Selected Customer";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Location = new System.Drawing.Point(87, 42);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.ReadOnly = true;
+            this.IdTextBox.Size = new System.Drawing.Size(131, 22);
+            this.IdTextBox.TabIndex = 4;
+            // 
+            // FullNameLabel
+            // 
+            this.FullNameLabel.AutoSize = true;
+            this.FullNameLabel.Location = new System.Drawing.Point(6, 70);
+            this.FullNameLabel.Name = "FullNameLabel";
+            this.FullNameLabel.Size = new System.Drawing.Size(75, 17);
+            this.FullNameLabel.TabIndex = 5;
+            this.FullNameLabel.Text = "Full Name:";
+            // 
+            // FullNameTextBox
+            // 
+            this.FullNameTextBox.Location = new System.Drawing.Point(87, 70);
+            this.FullNameTextBox.Name = "FullNameTextBox";
+            this.FullNameTextBox.Size = new System.Drawing.Size(359, 22);
+            this.FullNameTextBox.TabIndex = 6;
+            // 
+            // AddressLabel
+            // 
+            this.AddressLabel.AutoSize = true;
+            this.AddressLabel.Location = new System.Drawing.Point(6, 105);
+            this.AddressLabel.Name = "AddressLabel";
+            this.AddressLabel.Size = new System.Drawing.Size(64, 17);
+            this.AddressLabel.TabIndex = 7;
+            this.AddressLabel.Text = "Address:";
+            // 
+            // AddresTextBox
+            // 
+            this.AddresTextBox.Location = new System.Drawing.Point(87, 98);
+            this.AddresTextBox.Multiline = true;
+            this.AddresTextBox.Name = "AddresTextBox";
+            this.AddresTextBox.Size = new System.Drawing.Size(359, 191);
+            this.AddresTextBox.TabIndex = 8;
+            // 
+            // CustomersTabs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SelectedCustomerPanel);
+            this.Controls.Add(this.CustomersPanel);
+            this.Name = "CustomersTabs";
+            this.Size = new System.Drawing.Size(831, 515);
+            this.CustomersPanel.ResumeLayout(false);
+            this.CustomersPanel.PerformLayout();
+            this.ButtonsPanel.ResumeLayout(false);
+            this.AddPanel.ResumeLayout(false);
+            this.RemovePanel.ResumeLayout(false);
+            this.SelectedCustomerPanel.ResumeLayout(false);
+            this.SelectedCustomerPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel CustomersPanel;
+        private System.Windows.Forms.Panel ButtonsPanel;
+        private System.Windows.Forms.Panel RemovePanel;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Panel AddPanel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.ListBox CustomersListBox;
+        private System.Windows.Forms.Label CustomersLabel;
+        private System.Windows.Forms.Panel SelectedCustomerPanel;
+        private System.Windows.Forms.TextBox AddresTextBox;
+        private System.Windows.Forms.Label AddressLabel;
+        private System.Windows.Forms.TextBox FullNameTextBox;
+        private System.Windows.Forms.Label FullNameLabel;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.Label SelectedCustomerLabel;
+        private System.Windows.Forms.Label IdLabel;
+    }
+}
