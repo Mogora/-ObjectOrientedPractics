9ef856cf3d6f2a44e8f27f83cc5938d04fe48263
Mogora <67151047+Mogora@users.noreply.github.com>
2023-10-06 00:24:04 +0300

    Реализован класс Store

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Store.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные о покупателях и магазинах.
+    /// </summary>
+    public class Store
+    {
+        /// <summary>
+        /// Коллекция товаров.
+        /// </summary>
+        private List<Item> _items;
+
+        /// <summary>
+        /// Коллекция покупателей.
+        /// </summary>
+        private List<Customer> _customers;
+
+        public Store()
+        {
+            Items = new List<Item>();
+            Customers = new List<Customer>();
+        }
+
+        /// <summary>
+        /// Возвращает и задает товар.
+        /// </summary>
+        public List<Item> Items
+        {
+            get
+            {
+                return _items;
+            }
+            set
+            {
+                _items = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает покупателя. 
+        /// </summary>
+        public List<Customer> Customers
+        {
+            get
+            {
+                return _customers;
+            }
+            set
+            {
+                _customers = value;
+            }
+        }
+    }
+}
